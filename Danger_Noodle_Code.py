db89544c2ebf93f7e5b219125792b684440a998b
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-08-13 13:05:29 -0700

    exported from Skill Struck

---
+++ Danger_Noodle_Code.py
@@ -0,0 +1,13 @@
+ask = input("please pute in three numbers: ")
+
+num1 = ask[1]
+
+num2 = ask[2]
+
+num3 =ask[3]
+
+total = int(num1) + int(num2) + int(num3)
+
+birthday = "The sum of those digits is {}"
+
+print(birthday.format(total))
